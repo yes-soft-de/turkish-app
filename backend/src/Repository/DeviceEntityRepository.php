ab2a986cca98e8197cbe08c2dbfa92e84bc59de2
ralsalloum <ramisalloum12@hotmail.com>
2021-01-24 01:10:38 +0200

    username was being added to Get all devices

--- backend/src/Repository/DeviceEntityRepository.php
+++ backend/src/Repository/DeviceEntityRepository.php
@@ -43,7 +43,9 @@ class DeviceEntityRepository extends ServiceEntityRepository
     public function getAllDevices()
     {
         return $this->createQueryBuilder('device')
-             ->select('device.id', 'device.specialLink', 'device.image','device.brand', 'device.type', 'device.cpu', 'device.ram','device.battery', 'device.price', 'device.yearOfRelease','device.description', 'device.status', 'device.createdAt','device.updateAt','device.country', 'device.city','device.durationOfUse', 'device.gauge', 'UserProfileEntity.image as imageUser')
+             ->select('device.id', 'device.specialLink', 'device.image','device.brand', 'device.type', 'device.cpu', 'device.ram','device.battery',
+                 'device.price', 'device.yearOfRelease','device.description', 'device.status', 'device.createdAt','device.updateAt','device.country',
+                 'device.city','device.durationOfUse', 'device.gauge', 'UserProfileEntity.image as imageUser', 'UserProfileEntity.userName')
 
              ->leftJoin(
                 UserProfileEntity::class,
