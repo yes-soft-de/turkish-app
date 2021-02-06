ec352d4db9210ea8cecf880b8d871775379d630f
ralsalloum <ramisalloum12@hotmail.com>
2021-02-06 18:55:34 +0200

    Report Entity was being created

---
+++ backend/src/Entity/ReportEntity.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReportEntityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ReportEntityRepository::class)
+ */
+class ReportEntity
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $userID;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $entity;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $itemID;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $reason;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserID(): ?string
+    {
+        return $this->userID;
+    }
+
+    public function setUserID(string $userID): self
+    {
+        $this->userID = $userID;
+
+        return $this;
+    }
+
+    public function getEntity(): ?string
+    {
+        return $this->entity;
+    }
+
+    public function setEntity(string $entity): self
+    {
+        $this->entity = $entity;
+
+        return $this;
+    }
+
+    public function getItemID(): ?string
+    {
+        return $this->itemID;
+    }
+
+    public function setItemID(string $itemID): self
+    {
+        $this->itemID = $itemID;
+
+        return $this;
+    }
+
+    public function getReason(): ?string
+    {
+        return $this->reason;
+    }
+
+    public function setReason(string $reason): self
+    {
+        $this->reason = $reason;
+
+        return $this;
+    }
+}
