d8ac400d6833ba0ea82d26f52bdc2b9ae107317b
Zolfekar Seleten <zolfekarseleten94@gmail.com>
2021-02-14 11:27:06 +0200

    release-v1.0.2

--- hersay/lib/module_history/ui/state/history/history.state.dart
+++ hersay/lib/module_history/ui/state/history/history.state.dart
@@ -1,10 +1,9 @@
 
 import 'package:flutter/material.dart';
-import 'package:hersay/generated/l10n.dart';
 import 'package:hersay/module_auth/auth_routes.dart';
-import 'package:hersay/module_history/ui/widget/History_card/History_card.dart';
 import 'package:hersay/module_history/model/history/history_model.dart';
 import 'package:hersay/module_history/ui/screen/history/history_screen.dart';
+import 'package:hersay/module_history/ui/widget/history_card/history_card.dart';
 
 
 abstract class HistoryState {
