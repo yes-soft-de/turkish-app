581310570e445bfc7e8fb56f06cd382d613d0e93
ralsalloum <ramisalloum12@hotmail.com>
2021-03-21 20:40:31 +0200

    description field was being added

--- backend/src/Entity/ServicesEntity.php
+++ backend/src/Entity/ServicesEntity.php
@@ -22,6 +22,11 @@ class ServicesEntity
      */
     private $title;
 
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +40,18 @@ class ServicesEntity
     public function setTitle(string $title): self
     {
         $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
 
         return $this;
     }
